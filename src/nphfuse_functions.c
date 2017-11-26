638eb2fd6f1a2cfd66db8f3bdf3e26372ad90653
Sameer <spoudwa@ncsu.edu>
2017-11-26 17:26:43 -0500

    mkdir

--- src/nphfuse_functions.c
+++ src/nphfuse_functions.c
@@ -195,9 +195,9 @@ int nphfuse_mkdir(const char *path, mode_t mode)
         temp[index].mystat.st_uid = getuid();
         temp[index].mystat.st_gid = getgid();
         gettimeofday(&currTime, NULL);
-        pInodeInfo->fstat.st_atime = currTime.tv_sec;
-        pInodeInfo->fstat.st_mtime = currTime.tv_sec;
-        pInodeInfo->fstat.st_ctime = currTime.tv_sec;
+        temp[index].fstat.st_atime = currTime.tv_sec;
+        temp[index].fstat.st_mtime = currTime.tv_sec;
+        temp[index].fstat.st_ctime = currTime.tv_sec;
         return 0;
     }
     else
@@ -205,7 +205,6 @@ int nphfuse_mkdir(const char *path, mode_t mode)
         log_msg("Directory not found. \n");
         return -ENOENT;    
     }
-}
 
     return -ENOENT;
 }
