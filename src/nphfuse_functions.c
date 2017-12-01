d5acea9656ad4e3ec49f7e7d88feb0efbd6876d6
asinsinwal <assinsin@ncsu.edu>
2017-12-01 03:29:39 -0500

    trial mkdir

--- src/nphfuse_functions.c
+++ src/nphfuse_functions.c
@@ -731,7 +731,7 @@ int nphfuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t o
 
         for (index = 0; index < 32; index++){
             log_msg("Search directory %s and file %s\n", dir, filename);
-            log_msg("Current directory %s and file %s\n", temp[index]->dirname, temp[index]->filename);
+            //log_msg("Current directory %s and file %s\n", temp[index].dirname, temp[index].filename);
             if ((strcmp(temp[index].dirname, dir) == 0) && (strcmp(temp[index].filename, "/")!=0)){
                 /* Entry found in inode block */
                 log_msg("Adding into dirent.\n");
